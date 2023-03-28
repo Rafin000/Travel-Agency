1f2a8da55f64d51af9a6480405d1bf179950618c
Rafin000 <marufulislam00000@gmail.com>
2023-03-28 21:20:54 +0600

    resposive for main section

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,6 +5,14 @@ import { HiOutlineLocationMarker } from 'react-icons/hi'
 import { HiOutlineClipboardCheck } from 'react-icons/hi'
 
 import img from '../../Assets/img (1).jpg'
+import img1 from '../../Assets/img (2).jpg'
+import img2 from '../../Assets/img (3).jpg'
+import img3 from '../../Assets/img (4).jpg'
+import img4 from '../../Assets/img (5).jpg'
+import img5 from '../../Assets/img (6).jpg'
+import img6 from '../../Assets/img (7).jpg'
+import img7 from '../../Assets/img (8).jpg'
+import img8 from '../../Assets/img (9).jpg'
 
 const Data = [
     {
@@ -16,78 +24,78 @@ const Data = [
         fees: '$700',
         description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
     },
-    // {
-    //     id: 2,
-    //     imgSrc: 'img',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 3,
-    //     imgSrc: 'img3',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 4,
-    //     imgSrc: 'img1',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 5,
-    //     imgSrc: 'img1',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 6,
-    //     imgSrc: 'img1',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 7,
-    //     imgSrc: 'img1',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 8,
-    //     imgSrc: 'img1',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
-    // {
-    //     id: 9,
-    //     imgSrc: 'img1',
-    //     destTitle: 'Bora Bora',
-    //     location: 'New Zealand',
-    //     grade: 'CULTURAL RELAX',
-    //     fees: '$700',
-    //     description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
-    // },
+    {
+        id: 2,
+        imgSrc: img1,
+        destTitle: 'Machu Picchu',
+        location: 'Peru',
+        grade: 'CULTURAL RELAX',
+        fees: '$600',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 3,
+        imgSrc: img2,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 4,
+        imgSrc: img3,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 5,
+        imgSrc: img4,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 6,
+        imgSrc: img5,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 7,
+        imgSrc: img6,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 8,
+        imgSrc: img7,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
+    {
+        id: 9,
+        imgSrc: img8,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'THe epitome of romance,Bora Bora is one of the best travel destinations in the world. THis place is known for its luxurious stays and adventurous activities.'
+    },
 ]
 
 
@@ -105,8 +113,8 @@ const Main = () => {
                     Data.map(({ id, imgSrc, destTitle, location, grade, fees, description }) => {
                         return (
                             <div key={id} className="singleDestination">
-                                <div className="immageDiv">
-                                    <img src={img} alt={destTitle} />
+                                <div className="imageDiv">
+                                    <img src={imgSrc} alt={destTitle} />
                                 </div>
 
                                 <div className="cardInfo">
